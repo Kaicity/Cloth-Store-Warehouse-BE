36f645655a9841f68a8cd37a76f661a1fca3330d
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2024-03-22 17:52:08 +0700

    complete

--- ct-start/src/main/java/com/example/ctstart/configuration/WebMvcConfig.java
+++ ct-start/src/main/java/com/example/ctstart/configuration/WebMvcConfig.java
@@ -1,18 +1,25 @@
 package com.example.ctstart.configuration;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Value("${clientFE}")
+    private String clientFEPort;
+
+    @Value("${adminFE}")
+    private String adminFEPort;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println(clientFEPort);
         registry.addMapping("/api/**") // Đặt URL pattern của API của bạn ở đây
-                .allowedOrigins("http://localhost:4201", "http://localhost:4202")
+                .allowedOrigins(clientFEPort, adminFEPort)
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Cho phép các phương thức HTTP
                 .allowedHeaders("*"); // Cho phép tất cả các header
     }
